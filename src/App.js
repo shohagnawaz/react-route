992a616160b51f0473a541f774eaa80245feacd6
shohagnawaz <snawazdigital@gmail.com>
2020-10-26 15:45:18 +0600

    router-done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 function App() {
+
+  const myStyle = {
+    backgroundColor: "green",
+    color: "White",
+    margin: "40px",
+    padding: "20px"
+  }
+
   return (
     <div className="App">
-      <h1>Md. Shah Nawaz Shohag</h1>
+      <h1 style={myStyle}>Md. Shah Nawaz Shohag</h1>
+      <About></About>
+      <Contact></Contact>
+      <Portfolio></Portfolio>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
+import { Route, Link, BrowserRouter as Router } from '../node_modules/react-router-dom';
 import * as serviceWorker from './serviceWorker';
+
+const myrouter = (
+
+  <Router>
+    <div>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/portfolio">Portfolio</Link>
+      </ul>
+
+      <Route exact path="/" component={App}></Route>
+      <Route path="/about" component={About}></Route>
+      <Route path="/contact" component={Contact}></Route>
+      <Route path="/portfolio" component={Portfolio}></Route>
+    </div>
+  </Router>
+
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {myrouter}
   </React.StrictMode>,
   document.getElementById('root')
 );
